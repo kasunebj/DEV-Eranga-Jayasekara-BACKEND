b542a4f764c91c8b1faa89355124eef64de13101
kasunebj <kasunebj@gmail.com>
2019-01-19 11:47:23 +0530

    gps

---
+++ gps/src/main/java/com/ABC/gps/GpsApplication.java
@@ -0,0 +1,14 @@
+package com.ABC.gps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GpsApplication.class, args);
+	}
+
+}
+
---
+++ gps/src/test/java/com/ABC/gps/GpsApplicationTests.java
@@ -0,0 +1,17 @@
+package com.ABC.gps;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GpsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
